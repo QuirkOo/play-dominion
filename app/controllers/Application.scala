501843a041e5ed03acd3fc6abbce5459864c5b0f
yorg <nachteil@gmail.cp>
2015-01-28 17:48:23 +0100

    simple websocket echo

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,23 @@
 package controllers
 
-import play.api._
+import play.api.libs.iteratee.{Concurrent, Iteratee}
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index.render("Your new application is ready, biatch."))
+  }
+
+  def websocket =  WebSocket.using[String] { request =>
+
+    val (out,channel) = Concurrent.broadcast[String]
+
+    val in = Iteratee.foreach[String] {
+      msg => println(msg)
+        channel push("Got message: " + msg)
+    }
+    (in,out)
   }
 
 }
